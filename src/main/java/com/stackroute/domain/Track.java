d5745a0ea02c17eea41b94172d7eecc575c037e5
mites1997 <51950018+mites1997@users.noreply.github.com>
2019-07-29 15:22:30 +0530

    Update Track.java

--- src/main/java/com/stackroute/domain/Track.java
+++ src/main/java/com/stackroute/domain/Track.java
@@ -25,35 +25,5 @@ public class Track {
 
 
 
-   /* public Track(int id, String name, String comment) {
-        this.id = id;
-        this.name = name;
-        this.comment = comment;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-*/
 
 }
